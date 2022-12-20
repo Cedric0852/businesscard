aea4e9a26a4118b58e2c42a92114c72f2a58733d
Cedric0852 <mugishac777@gmail.com>
2022-12-20 16:16:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pytesseract
 from PIL import Image
-import os
-import re
-import csv
 
 st.title('OCR Tool FOR BUSINESS CARD V1.0 ')
 
@@ -13,6 +10,7 @@ def extract_text_from_image(image):
     # Extract the text from the image
     text = pytesseract.image_to_string(image)
     return text
+
 if uploaded_file is not None:
     # Open the image file
     image = Image.open(uploaded_file)
@@ -25,10 +23,9 @@ if uploaded_file is not None:
     st.write(text)
     # Add a text area widget to allow the user to edit the extracted text
     edited_text = st.text_area("Edit the extracted text:", value=text)
+
 if st.button("Save"):
     # Save the edited text to a file
     with open("edited_text.txt", "w") as f:
         f.write(edited_text)
     st.success("Text saved to file!")
-
-
